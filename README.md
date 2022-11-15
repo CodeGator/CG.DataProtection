dcb6b3162c20b2a6472cf3c5d37fcf6a77d928c2
Martin Cook <33993749+martycook@users.noreply.github.com>
2022-11-15 14:49:13 -0600

    refactoring

--- README.md
+++ README.md
@@ -31,4 +31,7 @@ We have a discussion group [HERE](https://github.com/CodeGator/CG.DataProtection
 There is developer documentation [HERE](https://codegator.github.io/CG.DataProtection/)
 
 We also blog about projects like this one on our website, [HERE](http://www.codegator.com)
+---
+#### Disclaimer
+This package and it's contents are experimental in nature. There is no official support. Use at your own risk.
 
---
+++ src/CG.DataProtection/Options/AzureKeyStorageOptions.cs
@@ -0,0 +1,34 @@
+﻿
+namespace CG.DataProtection.Options;
+
+/// <summary>
+/// This class represents configuration settings for Azure key storage.
+/// </summary>
+internal class AzureKeyStorageOptions
+{
+    // *******************************************************************
+    // Properties.
+    // *******************************************************************
+
+    #region Properties
+
+    /// <summary>
+    /// This property contains a connection string for azure BLOB storage.
+    /// </summary>
+    [Required]
+    public string ConnectionString { get; set; } = null!;
+
+    /// <summary>
+    /// This property contains a container name for azure BLOB storage.
+    /// </summary>
+    [Required]
+    public string ContainerName { get; set; } = null!;
+
+    /// <summary>
+    /// This property contains a BLOB name for key storage.
+    /// </summary>
+    [Required]
+    public string BlobName { get; set; } = null!;
+
+    #endregion
+}
